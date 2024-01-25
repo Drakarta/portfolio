e10c543b760e4ee7a6b481c6fff80d465d3b0046
Drakarta <Drakarta3@gmail.com>
2024-01-25 04:06:30 +0100

    mobile view improvement

--- frontend/src/components/sections/HomeSection.tsx
+++ frontend/src/components/sections/HomeSection.tsx
@@ -21,91 +21,89 @@ function HomeSection() {
 	const firstActivate = useStore($firstActivate);
 	return (
 		<>
-			<div className="flex h-screen flex-col items-center justify-center">
-				{isMobile ? (
-					<>
+			{isMobile ? (
+				<div className="grid h-screen grid-cols-3 grid-rows-9">
+					<div
+						className="col-start-1 col-end-4 row-start-5 flex cursor-pointer justify-center"
+						onClick={() => {
+							$nameActive.set(!nameActive);
+							$firstActivate.set(true);
+						}}
+					>
+						<img
+							className="-my-4 aspect-square w-32"
+							style={{ filter: "drop-shadow(3px 3px 5px black)" }}
+							src={ant.src}
+							alt="ant"
+						/>
+						<img
+							className="-my-4 aspect-square w-32"
+							style={{ filter: "drop-shadow(3px 3px 5px black)" }}
+							src={honey.src}
+							alt="honey_pot"
+						/>
+					</div>
+					<motion.div
+						className="pointer-events-none relative top-6 col-start-2 row-start-6 flex justify-center gap-4 text-lg opacity-30"
+						style={{ filter: "drop-shadow(1px 1px 2px black)" }}
+						initial={false}
+						animate={firstActivate ? { opacity: 0 } : "initial"}
+					>
+						<FontAwesomeIcon icon={faArrowUpLong} />
+						<div className="text-lg">Tap</div>
+						<FontAwesomeIcon icon={faArrowUpLong} />
+					</motion.div>
+					<motion.div
+						onClick={() => {
+							$nameActive.set(!nameActive);
+						}}
+						className="relative col-start-1 col-end-4 row-start-6 row-end-8 m-2 flex max-h-20 w-fit cursor-pointer justify-center justify-self-center overflow-hidden px-1.5 text-6xl"
+						style={{ lineHeight: "normal", letterSpacing: "-0.025em" }}
+						initial={{ height: "0px" }}
+						animate={nameActive ? { height: "auto" } : "initial"}
+						transition={{ duration: 0.5, ease: "easeInOut" }}
+					>
 						<div
-							className="flex cursor-pointer justify-center"
-							onClick={() => {
-								$nameActive.set(!nameActive);
-								$firstActivate.set(true);
-							}}
+							className="bespoke-sans"
+							style={{ filter: "drop-shadow(3px 3px 5px black" }}
 						>
-							<img
-								className="-my-4 aspect-square w-32"
-								style={{ filter: "drop-shadow(3px 3px 5px black)" }}
-								src={ant.src}
-								alt="ant"
-							/>
-							<img
-								className="-my-4 aspect-square w-32"
-								style={{ filter: "drop-shadow(3px 3px 5px black)" }}
-								src={honey.src}
-								alt="honey_pot"
-							/>
+							Anthon
 						</div>
 						<motion.div
-							className="pointer-events-none relative top-6 flex justify-center gap-4 text-lg opacity-30"
-							style={{ filter: "drop-shadow(1px 1px 2px black)" }}
+							className="bespoke-sans"
+							style={{ filter: "drop-shadow(3px 3px 5px black" }}
 							initial={false}
-							animate={firstActivate ? { opacity: 0 } : "initial"}
-						>
-							<FontAwesomeIcon icon={faArrowUpLong} />
-							<div className="text-lg">Tap</div>
-							<FontAwesomeIcon icon={faArrowUpLong} />
-						</motion.div>
-						<motion.div
-							onClick={() => {
-								$nameActive.set(!nameActive);
+							animate={
+								nameActive
+									? {
+											y: ["0rem", "4rem", "4rem"],
+											width: ["2rem", "2rem", "0rem"],
+										}
+									: "initial"
+							}
+							transition={{
+								delay: 0.375,
+								duration: nameActive ? 0.75 : 0,
+								ease: "easeInOut",
+								times: [0.3, 0.6, 0.9],
 							}}
-							className="relative -top-6 flex cursor-pointer justify-center overflow-hidden px-1.5 text-6xl"
-							style={{ lineHeight: "normal", letterSpacing: "-0.025em" }}
-							initial={{ height: "0px" }}
-							animate={nameActive ? { height: "auto" } : "initial"}
-							transition={{ duration: 0.5, ease: "easeInOut" }}
 						>
-							<div
-								className="bespoke-sans"
-								style={{ filter: "drop-shadow(3px 3px 5px black" }}
-							>
-								Anthon
-							</div>
-							<motion.div
-								className="bespoke-sans"
-								style={{ filter: "drop-shadow(3px 3px 5px black" }}
-								initial={false}
-								animate={
-									nameActive
-										? {
-												y: ["0rem", "4rem", "4rem"],
-												width: ["2rem", "2rem", "0rem"],
-											}
-										: "initial"
-								}
-								transition={{
-									delay: 0.375,
-									duration: nameActive ? 0.75 : 0,
-									ease: "easeInOut",
-									times: [0.3, 0.6, 0.9],
-								}}
-							>
-								e
-							</motion.div>
-							<div
-								className="bespoke-sans"
-								style={{ filter: "drop-shadow(3px 3px 5px black" }}
-							>
-								y
-							</div>
+							e
 						</motion.div>
-					</>
-				) : (
-					<>
-						<AntHoney />
-						<NameHoverIndicator />
-					</>
-				)}
-			</div>
+						<div
+							className="bespoke-sans"
+							style={{ filter: "drop-shadow(3px 3px 5px black" }}
+						>
+							y
+						</div>
+					</motion.div>
+				</div>
+			) : (
+				<div className="flex h-screen flex-col items-center justify-center">
+					<AntHoney />
+					<NameHoverIndicator />
+				</div>
+			)}
 			<ScrollIndicator />
 		</>
 	);
--- frontend/src/resources/store.ts
+++ frontend/src/resources/store.ts
@@ -1,7 +1,5 @@
 import { atom, } from "nanostores";
 
-
-
 export const $isMobile = atom<boolean>(false);
 export const $nameActive = atom<boolean>(false);
 export const $firstActivate = atom<boolean>(false);
