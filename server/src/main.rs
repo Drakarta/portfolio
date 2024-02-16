bb8f45fe9ebf9c40b8eb2e503c6335e33a66b931
Drakarta <Drakarta3@gmail.com>
2024-02-16 13:47:05 +0100

    rust axum api

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,29 +1,18 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
+use axum::{routing::get, Router};
 
-async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let mut buffer = [0; 1024];
-
-    let bytes_read = stream.read(&mut buffer).await?;
-    println!(
-        "Received: {}",
-        String::from_utf8_lossy(&buffer[..bytes_read])
-    );
-
-    stream.write_all(b"Hello from Tokio!\n").await?;
-    Ok(())
-}
+use std::net::SocketAddr;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("0.0.0.0:3000").await?;
-    println!("Server listening on http://0.0.0.0:3000");
-    loop {
-        let (stream, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            if let Err(e) = handle_client(stream).await {
-                eprintln!("Error handling client: {}", e);
-            }
-        });
-    }
+async fn main() {
+    let app = Router::new()
+        // Root router
+        .route("/", get(root));
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running on http://{address}",);
+    let listener = tokio::net::TcpListener::bind(&address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
